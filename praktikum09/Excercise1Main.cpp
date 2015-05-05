e950b43cc63ac54b574c2cfe5aa276c4dcf7a959
ChaosBelcebub <mich3ael@web.de>
2015-05-05 23:09:02 +0200

    add excercise 1

---
+++ praktikum09/Excercise1Main.cpp
@@ -0,0 +1,11 @@
+// Aufgabe 1
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  // Ausgabe der Anzahl von parametern
+  cout << "Anzahl der Argumente nach dem Scriptnamen:" << endl;
+  cout << argc - 1 << endl;
+}

cb59351cf8a4e8911461f11bfb1bec65be87f1cf
ChaosBelcebub <mich3ael@web.de>
2015-05-06 01:10:31 +0200

    Finished version of excercise 3

--- praktikum09/Excercise3Main.cpp
+++ praktikum09/Excercise3Main.cpp
@@ -6,9 +6,40 @@
 using namespace std;
 
 bool field[8][8];
+int destX;
+int destY;
 
 bool moveKnight(int posX, int posY)
 {
+  // Prüfe ob die Werte im Feld liegen
+  if (posX < 0 || posX > 7 ||  posY < 0 || posY > 7)
+  {
+    return false;
+  }
+  // Prüfen ob das ziel erreicht wurde
+  else if (posX == destX && posY == destY)
+  {
+    // cout << "Treffer!" << endl;
+    return true;
+  }
+  // Prüfen ob das Feld bereits erreicht wurde
+  else if (field[posX][posY] == true)
+  {
+    return false;
+  }
+  else
+  {
+    // Aufrufen der möglichekeiten
+    field[posX][posY] = true;
+    moveKnight(posX + 2, posY + 1);
+    moveKnight(posX + 2, posY - 1);
+    moveKnight(posX - 2, posY + 1);
+    moveKnight(posX - 2, posY - 1);
+    moveKnight(posX + 1, posY + 2);
+    moveKnight(posX + 1, posY - 2);
+    moveKnight(posX - 1, posY + 2);
+    moveKnight(posX - 1, posY - 2);
+  }
   return true;
 }
 
@@ -42,7 +73,17 @@ int main(int argc, char *argv[])
       }
     }
 
+    destX = atoi(argv[3]);
+    destY = atoi(argv[4]);
     moveKnight(atoi(argv[1]), atoi(argv[2]));
 
   }
 }
+
+/* 1. Ja, auf jeden Fall. Irgendwann existieren keine felder mehr um ein
+ *    Feld das noch nie besucht wurde und die Funktion springt eine Ebene höher
+ *    bis die Rekursion beendet ist.
+ * 2. Ja
+ * 3. Es wird mindestens 1 Weg gefunden
+ * 4. Schwer aufzuschreiben
+ */
